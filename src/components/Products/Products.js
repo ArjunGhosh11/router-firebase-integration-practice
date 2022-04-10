e97eceb74517768116268b3bdfd603fed3109a3b
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-04-10 20:07:56 +0600

    Custom hooks introduced

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,15 +1,15 @@
 import { getAuth } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-const auth = getAuth()
-const Home = () => {
+const auth = getAuth();
+const Products = () => {
     const [user] = useAuthState(auth);
     return (
         <div>
-            <h2>This is home</h2>
-            <p>Current User is: {user ? user.displayName : 'Nobody'}</p>
+            <h2>Knock Knock!! Whis is there?</h2>
+            <h5>{user ? user.displayName : 'Bhoot'}</h5>
         </div>
     );
 };
 
-export default Home;
+export default Products;
