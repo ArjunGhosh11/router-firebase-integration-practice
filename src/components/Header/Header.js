6dde808ec26547ea95d7b33a020dc78ab39136f9
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-04-10 19:14:50 +0600

    added onAuth

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import useFirebase from '../../hooks/useFirebase';
 import './Header.css'
 const Header = () => {
-    const { user } = useFirebase()
+    const { user, handleSignOut } = useFirebase()
     return (
         <div className='header'>
             <nav>
@@ -11,9 +11,11 @@ const Header = () => {
                 <Link to='/products'>Products</Link>
                 <Link to='/orders'>Orders</Link>
                 <Link to='/register'>Register</Link>
+                <span>{user?.displayName && user.displayName}</span>
                 {
-                    user.uid ?
-                        <button>SignOut</button>
+                    user?.uid
+                        ?
+                        <button onClick={handleSignOut}>SignOut</button>
                         :
                         <Link to='/login'>Login</Link>
                 }
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { useEffect, useState } from "react"
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth'
 import app from "../firebase,init";
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
@@ -15,10 +15,19 @@ const useFirebase = () => {
                 setUser(user);
             })
     }
-
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => { });
+    }
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            setUser(user);
+        })
+    }, [])
     return {
         user,
-        signInWithGoogle
+        signInWithGoogle,
+        handleSignOut
     };
 }
 
